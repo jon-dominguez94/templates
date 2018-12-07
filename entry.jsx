765a50ea59b8e84b20be2a5ff83719ecd212a1c0
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-07 07:41:53 -0800

    Add front end auth files

---
+++ entry.jsx
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const { currentUser } = window;
+    const { id } = currentUser;
+    const preloadedState = {
+      entitities: {
+        users: {
+          [id]: currentUser
+        }
+      }
+      session: { id }
+    };
+    store = configureStore(preloadedState);
+
+    // Clean up after ourselves so we don't accidentally use the
+    // global currentUser instead of the one in the store
+    delete window.currentUser;
+
+  } else {
+    store = configureStore();
+  }
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});
