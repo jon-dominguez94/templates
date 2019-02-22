5ee9ded024a0c5bc25809fb69a163391dd2f371b
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-21 22:59:15 -0800

    Write add counter thunk action

--- rr/src/actions/counter_actions.js
+++ rr/src/actions/counter_actions.js
@@ -1,7 +1,11 @@
 export const ADD_COUNTER = "ADD_COUNTER";
 
-export const addCounter = () => {
+export const add = () => {
   return ({
     type: ADD_COUNTER
   });
+};
+
+export const addCounter = () => dispatch => {
+  return dispatch(add());
 };
