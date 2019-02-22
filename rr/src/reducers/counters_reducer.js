b828f1236283db956fae0a8e3601d451702b999c
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-21 23:14:11 -0800

    Write counters reducer

--- rr/src/reducers/counters_reducer.js
+++ rr/src/reducers/counters_reducer.js
@@ -1,6 +1,10 @@
+import { ADD_COUNTER } from '../actions/counter_actions';
+
 const countersReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type){
+    case ADD_COUNTER:
+      
     default:
       return state;
   }
