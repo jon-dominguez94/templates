cd7be380bb1aac354aed5dc527ec0e852d41d332
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-22 23:41:50 -0800

    Add counters to entities reducer

--- rr/src/reducers/entities_reducer.js
+++ rr/src/reducers/entities_reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import counters from './counter_reducer';
 
 const entitiesReducer = combineReducers({
-
+  counters
 });
 
 export default entitiesReducer;
