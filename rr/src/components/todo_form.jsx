d6701c89622f510655eabd816c76df30f2b369d4
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-22 23:29:44 -0800

    Finish todo form skeleton

--- rr/src/components/todo_form.jsx
+++ rr/src/components/todo_form.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 class TodoForm extends React.Component {
+  constructor(props){
+    super(props);
 
+    this.state = {
+      body: ''
+    };
+  }
+
+  render() {
+    <div>
+      <form>
+        <input type="text" placeholder="body"/>
+      </form>
+    </div>
+  }
 }
 
 export default TodoForm;
