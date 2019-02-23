02b13f9a3546c7ec89df60486cdbc3014ce58e61
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-22 23:31:23 -0800

    Write todo form container skeleton

--- rr/src/components/todo_form_container.js
+++ rr/src/components/todo_form_container.js
@@ -1,1 +1,16 @@
 import { connect } from 'react-redux';
+import TodoForm from './todo_form';
+
+const mstp = state => {
+  return ({
+
+  });
+};
+
+const mdtp = dispatch => {
+  return ({
+
+  });
+};
+
+export default connect(mstp, mdtp)(TodoForm);
