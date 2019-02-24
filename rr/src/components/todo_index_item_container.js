c91400093138625edaccfa076a468fc83480c81d
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-23 17:42:26 -0800

    Give index item update prop

--- rr/src/components/todo_index_item_container.js
+++ rr/src/components/todo_index_item_container.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import TodoIndexItem from './todo_index_item';
+import { updateTodo } from '../actions/todo_actions';
 
 const mstp = (state, ownProps) => {
   return({
@@ -9,7 +10,7 @@ const mstp = (state, ownProps) => {
 
 const mdtp = dispatch => {
   return ({
-
+    updateTodo: todo => dispatch(updateTodo(todo))
   });
 };
 
