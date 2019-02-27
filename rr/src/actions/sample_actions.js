86902a90903ec3485528d7a4fc0053a4a2ae90bc
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-27 10:43:17 -0800

    Remove boilerplate code

--- rr/src/actions/sample_actions.js
+++ rr/src/actions/sample_actions.js
@@ -1,19 +1,2 @@
 import * as APIUtil from '../util/api_util';
 
-export const RECEIVE_SAMPLE = "RECEIVE_SAMPLE";
-
-const receiveSample = sample => {
-  return ({
-    type: RECEIVE_SAMPLE,
-    sample
-  });
-};
-
-export const createSample = sample => dispatch => {
-  try {
-    const newSample = APIUtil.createSample(sample);
-    dispatch(receiveSample(newSample));
-  } catch(err) {
-    console.log(err);
-  }
-};
