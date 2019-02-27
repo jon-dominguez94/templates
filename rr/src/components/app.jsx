0ebe449457167ac5c3c89cf4b6cc9bd5aceb6c76
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-27 10:36:53 -0800

    Clear out app file

--- rr/src/components/app.jsx
+++ rr/src/components/app.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import TodoFormContainer from './todo_form_container';
-import TodoIndexContainer from './todo_index_container';
 
 const App = () => (
   <div>
-    <TodoFormContainer />
-    <TodoIndexContainer />
+
   </div>
 );
 
